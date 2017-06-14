76d2f75ae842e33a0f2208140322a2cfdd137bc2
Nifrez <11frez11@gmail.com>
2017-06-14 13:00:33 +0200

    Added delay

--- background.js
+++ background.js
@@ -1,25 +1,32 @@
 chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    console.log(sender.tab ?
-                "from a content script:" + sender.tab.url :
-                "from the extension");
+    function (request, sender, sendResponse) {
+        console.log(sender.tab ?
+            "from a content script:" + sender.tab.url :
+            "from the extension");
 
-    if (request.chatFound) {
-      chrome.pageAction.show(sender.tab.id);
-      chrome.pageAction.setTitle({tabId: sender.tab.id, title: "Loots Auto-Clicker: Active!"});
-      sendResponse({registered: true});
-    }
-
-
-    if (request.linkFound) {
-      chrome.tabs.create({"url": request.linkURL, "active": false},
-        function(tab) {
-          setTimeout( function() {
-            chrome.tabs.remove(tab.id);
-          }, 10000)
+        if (request.chatFound) {
+            chrome.pageAction.show(sender.tab.id);
+            chrome.pageAction.setTitle({tabId: sender.tab.id, title: "Loots Auto-Clicker: Active!"});
+            sendResponse({registered: true});
         }
-      )
-      sendResponse({loots: true});
-    }
 
-  });
+
+        if (request.linkFound) {
+
+            var delay = Math.floor((Math.random() * 3000) + 2000);
+            var timeout = Math.floor((Math.random() * 4000) + 8000);
+
+            setTimeout(function(){
+                chrome.tabs.create({"url": request.linkURL, "active": false},
+                    function (tab) {
+                        setTimeout(function () {
+                            chrome.tabs.remove(tab.id);
+                        }, timeout)
+                    }
+                )
+            }, delay);
+            sendResponse({loots: true});
+        }
+
+    });
+
